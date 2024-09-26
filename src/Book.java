a1a4057e7e1091a240909c9ea7a04c3989b6e4ed
LAAnderson <logananderson@arizona.edu>
2024-09-26 15:59:51 -0700

    what should be the final version of book and library node

--- src/Book.java
+++ src/Book.java
@@ -1,25 +1,25 @@
 /**
  * @author Logan Anderson
  * 
- * represents a book
+ *         represents a book
  */
 public final class Book {
 	public Book(String t, String a) {
 		title = t;
 		author = a;
 	}
-	
+
 	private String title;
 	private String author;
-	
+
 	public String getTitle() {
 		return this.title;
 	}
-	
+
 	public String getAuthor() {
 		return this.author;
 	}
-	
+
 	@Override
 	public String toString() {
 		return this.title + " by " + this.author;
--- src/LibraryNode.java
+++ src/LibraryNode.java
@@ -3,8 +3,8 @@ import java.util.Comparator;
 /**
  * @author Logan Anderson
  * 
- * contains the relevant information regarding a book, 
- * including itself, its rating, and if it's read
+ *         contains the relevant information regarding a book, including itself,
+ *         its rating, and if it's read
  */
 public class LibraryNode {
 	public LibraryNode(String title, String author) {
@@ -12,30 +12,30 @@ public class LibraryNode {
 		this.rating = 0;
 		this.isRead = false;
 	}
-	
+
 	private Book book;
 	private int rating;
 	private boolean isRead;
-	
+
 	public Book getBook() {
 		return this.book;
 	}
-	
+
 	public int getRating() {
 		return this.rating;
 	}
-	
+
 	/**
 	 * @pre r must be [1 .. 5]
 	 */
 	public void setRating(int r) {
 		this.rating = r;
 	}
-	
+
 	public boolean getIsRead() {
 		return this.isRead;
 	}
-	
+
 	public void read() {
 		this.isRead = true;
 	}
@@ -44,18 +44,18 @@ public class LibraryNode {
 		@Override
 		public int compare(LibraryNode l1, LibraryNode l2) {
 			return l1.getBook().getAuthor().compareTo(l2.getBook().getAuthor());
-		}		
+		}
 	}
-	
+
 	static class compareByTitle implements Comparator<LibraryNode> {
 		@Override
 		public int compare(LibraryNode l1, LibraryNode l2) {
 			return l1.getBook().getTitle().compareTo(l2.getBook().getTitle());
 		}
 	}
-	
+
 	@Override
 	public String toString() {
-		return this.book.toString();
+		return this.book.toString() + ", Read: " + this.isRead + ", Rating: " + this.rating + "/5";
 	}
 }
