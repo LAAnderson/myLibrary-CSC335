7816d5c2136d68eca799f0c4f0204f35a0192f6d
carsonheyman <stiz@DESKTOP-ARMP5TF>
2024-10-01 20:57:28 -0700

    Fixed merging mess

--- src/MyLibrary.java
+++ src/MyLibrary.java
@@ -1,5 +1,5 @@
-import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * @authors Carson Heyman, Logan Anderson
@@ -24,9 +24,11 @@ public class MyLibrary {
 		 * 
 		 */
 		while (!userInput.equals("exit")) {
+
 			if (userInput.equals("search")) { // Carson
 				while (!userInput.equals("exit")) {
-					System.out.println("Enter the number representing the search method [1 .. 3]\n1 - title\n2 - author\n3 - rating");
+					System.out.println(
+							"Enter the number representing the search method [1 .. 3]\n1 - title\n2 - author\n3 - rating");
 					userInput = s.nextLine();
 					String option = userInput;
 
@@ -42,7 +44,7 @@ public class MyLibrary {
 						System.out.println("\u001B[31m" + "Invalid input!" + "\u001B[0m");
 						continue;
 					}
-					
+
 					userInput = s.nextLine();
 					String search = userInput;
 					int optionInt = Integer.parseInt(option);
@@ -51,13 +53,32 @@ public class MyLibrary {
 					break;
 				}
 			} else if (userInput.equals("addbook")) { // Logan
+				System.out.print("title: ");
+				String t = s.nextLine().toLowerCase();
+				System.out.print("author: ");
+				String a = s.nextLine().toLowerCase();
+
+				controller.addBook(t, a);
+				System.out.println("\u001B[32m" + "Successfully added '" + t + "', by '" + a + "'" + "\u001B[0m");
 
 			} else if (userInput.equals("settoread")) { // Carson
 				System.out.println("Enter the book title: ");
 				String title = s.nextLine();
 				controller.setToRead(title);
-				System.out.println("\u001B[32m"+ "Successfully set '" + title + "' to read." + "\u001B[0m");
+				System.out.println("\u001B[32m" + "Successfully set '" + title + "' to read." + "\u001B[0m");
+
 			} else if (userInput.equals("rate")) { // Logan
+				System.out.print("title: ");
+				String t = s.nextLine().toLowerCase();
+				System.out.print("rating [1 - 5]: ");
+				int r = Integer.parseInt(s.nextLine());
+
+				try {
+					controller.rate(t, r);
+					System.out.println("\u001B[32m" + "Rating added successfully" + "\u001B[0m");
+				} catch (Exception ArrayIndexOutOfBoundsException) {
+					System.out.println("\u001B[31m" + "Book not in library." + "\u001B[0m");
+				}
 
 			} else if (userInput.equals("getbooks")) { // Carson
 				while (!userInput.equals("exit")) {
@@ -78,15 +99,23 @@ public class MyLibrary {
 					break;
 				}
 			} else if (userInput.equals("suggestread")) { // Logan
+				System.out.println(controller.suggestRead().getBook().getTitle() + ", by "
+						+ controller.suggestRead().getBook().getAuthor());
 
 			} else if (userInput.equals("addbooks")) { // Carson
-				
-			} else if (userInput.equals("help")) { // Logan
 
+			} else if (userInput.equals("help")) { // Logan
+				System.out.println("search: \tsearch for a book by title (0), author (1), or rating(2)\n"
+						+ "addBook:\tadd a book to your library\n" + "setToRead:\tset a book in your library to read\n"
+						+ "rate:\t\trate a book in your library\n"
+						+ "getBooks:\tdisplay a list of books sorted by title (0), author (1), read books (2), or unread books (3)\n"
+						+ "suggestRead:\tsuggest and unread book\n"
+						+ "addBooks:\tadd multiple books from a file formatted as (title;author)\n"
+						+ "exit:\t\tterminate library");
 			} else {
 				System.out.println("\u001B[31m" + "Invalid Command. Try 'help'" + "\u001B[0m");
 			}
-			
+
 			System.out.println("Enter a command: ");
 			userInput = s.nextLine().toLowerCase();
 		}
