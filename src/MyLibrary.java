5a2db52b3192cb7dec8b6f8c54bae892b24ea849
LAAnderson <logananderson@arizona.edu>
2024-10-01 19:32:48 -0700

    forgot to format :P

--- src/MyLibrary.java
+++ src/MyLibrary.java
@@ -24,10 +24,11 @@ public class MyLibrary {
 		 * 
 		 */
 		while (!userInput.equals("exit")) {
-			
+
 			if (userInput.equals("search")) { // Carson
 				while (!userInput.equals("exit")) {
-					System.out.println("Enter the number representing the search method [1 .. 3]\n1 - title\n2 - author\n3 - rating");
+					System.out.println(
+							"Enter the number representing the search method [1 .. 3]\n1 - title\n2 - author\n3 - rating");
 					userInput = s.nextLine();
 					String option = userInput;
 
@@ -43,7 +44,7 @@ public class MyLibrary {
 						System.out.println("\u001B[31m" + "Invalid input!" + "\u001B[0m");
 						continue;
 					}
-					
+
 					userInput = s.nextLine();
 					String search = userInput;
 					int optionInt = Integer.parseInt(option);
@@ -56,44 +57,44 @@ public class MyLibrary {
 				String t = s.nextLine().toLowerCase();
 				System.out.print("author: ");
 				String a = s.nextLine().toLowerCase();
-				
+
 				controller.addBook(t, a);
-				System.out.println("\u001B[32m"+ "Successfully added '" + t + "', by '" + a + "'" + "\u001B[0m");
-				
+				System.out.println("\u001B[32m" + "Successfully added '" + t + "', by '" + a + "'" + "\u001B[0m");
+
 			} else if (userInput.equals("settoread")) { // Carson
 				System.out.println("Enter the book title: ");
 				String title = s.nextLine();
 				controller.setToRead(title);
-				System.out.println("\u001B[32m"+ "Successfully set '" + title + "' to read." + "\u001B[0m");
-				
+				System.out.println("\u001B[32m" + "Successfully set '" + title + "' to read." + "\u001B[0m");
+
 			} else if (userInput.equals("rate")) { // Logan
 				System.out.print("title: ");
 				String t = s.nextLine().toLowerCase();
 				System.out.print("rating [1 - 5]: ");
 				int r = Integer.parseInt(s.nextLine());
-				
+
 				try {
 					controller.rate(t, r);
-					System.out.println("\u001B[32m"+ "Rating added successfully" + "\u001B[0m");
-				} catch(Exception ArrayIndexOutOfBoundsException) {
+					System.out.println("\u001B[32m" + "Rating added successfully" + "\u001B[0m");
+				} catch (Exception ArrayIndexOutOfBoundsException) {
 					System.out.println("\u001B[31m" + "Book not in library." + "\u001B[0m");
 				}
 
 			} else if (userInput.equals("getbooks")) { // Carson
 				ArrayList<LibraryNode> books = controller.getBooks(0);
-				for(LibraryNode l : books) {
+				for (LibraryNode l : books) {
 					System.out.println(l.toString());
 				}
 
 			} else if (userInput.equals("suggestread")) { // Logan
-				System.out.println(controller.suggestRead().getBook().getTitle() + ", by " + controller.suggestRead().getBook().getAuthor());
+				System.out.println(controller.suggestRead().getBook().getTitle() + ", by "
+						+ controller.suggestRead().getBook().getAuthor());
 
 			} else if (userInput.equals("addbooks")) { // Carson
 
 			} else if (userInput.equals("help")) { // Logan
 				System.out.println("search: \tsearch for a book by title (0), author (1), or rating(2)\n"
-						+ "addBook:\tadd a book to your library\n"
-						+ "setToRead:\tset a book in your library to read\n"
+						+ "addBook:\tadd a book to your library\n" + "setToRead:\tset a book in your library to read\n"
 						+ "rate:\t\trate a book in your library\n"
 						+ "getBooks:\tdisplay a list of books sorted by title (0), author (1), read books (2), or unread books (3)\n"
 						+ "suggestRead:\tsuggest and unread book\n"
@@ -105,7 +106,7 @@ public class MyLibrary {
 				System.out.println("\u001B[31m" + "Invalid Command. Try 'help'" + "\u001B[0m");
 //>>>>>>> 949a540cfb222e5cb576a89fdb2a029d3e5b783e
 			}
-			
+
 			System.out.println("Enter a command: ");
 			userInput = s.nextLine().toLowerCase();
 		}
--- src/MyLibraryModel.java
+++ src/MyLibraryModel.java
@@ -6,135 +6,134 @@ import java.util.Scanner;
 /**
  * @authors Carson Heyman, Logan Anderson
  * 
- * Model for the library. Contains various searching and sorting
- * methods.
+ *          Model for the library. Contains various searching and sorting
+ *          methods.
  */
 public class MyLibraryModel {
 
-  private ArrayList<LibraryNode> library;
+	private ArrayList<LibraryNode> library;
 
-  MyLibraryModel() {
-    library = new ArrayList<LibraryNode>();
-  }
+	MyLibraryModel() {
+		library = new ArrayList<LibraryNode>();
+	}
 
-  public ArrayList<LibraryNode> findByRating(int rating) {
-    ArrayList<LibraryNode> found = new ArrayList<>();
-    for (LibraryNode node : library) {
-      if (node.getRating() == rating) {
-        found.add(node);
-      }
-    }
-    return found;
-  }
+	public ArrayList<LibraryNode> findByRating(int rating) {
+		ArrayList<LibraryNode> found = new ArrayList<>();
+		for (LibraryNode node : library) {
+			if (node.getRating() == rating) {
+				found.add(node);
+			}
+		}
+		return found;
+	}
 
-  public ArrayList<LibraryNode> findByTitle(String title) {
-    ArrayList<LibraryNode> found = new ArrayList<>();
-    for (LibraryNode node : library) {
-      if (node.getBook().getTitle().equals(title)) {
-        found.add(node);
-      }
-    }
-    return found;
-  }
+	public ArrayList<LibraryNode> findByTitle(String title) {
+		ArrayList<LibraryNode> found = new ArrayList<>();
+		for (LibraryNode node : library) {
+			if (node.getBook().getTitle().equals(title)) {
+				found.add(node);
+			}
+		}
+		return found;
+	}
 
-  public ArrayList<LibraryNode> findByAuthor(String author) {
-    ArrayList<LibraryNode> found = new ArrayList<>();
-    for (LibraryNode node : library) {
-      if (node.getBook().getAuthor().equals(author)) {
-        found.add(node);
-      }
-    }
-    return found;
-  }
+	public ArrayList<LibraryNode> findByAuthor(String author) {
+		ArrayList<LibraryNode> found = new ArrayList<>();
+		for (LibraryNode node : library) {
+			if (node.getBook().getAuthor().equals(author)) {
+				found.add(node);
+			}
+		}
+		return found;
+	}
 
-  public void addNode(String title, String author) {
-    LibraryNode newBook = new LibraryNode(title, author);
-    library.add(newBook);
-  }
+	public void addNode(String title, String author) {
+		LibraryNode newBook = new LibraryNode(title, author);
+		library.add(newBook);
+	}
 
-  public void setToRead(String title) {
-    ArrayList<LibraryNode> books = findByTitle(title);
-    if (books.size() >= 1) {
-      LibraryNode node = books.get(0);
-      node.read();
-    }
-  }
+	public void setToRead(String title) {
+		ArrayList<LibraryNode> books = findByTitle(title);
+		if (books.size() >= 1) {
+			LibraryNode node = books.get(0);
+			node.read();
+		}
+	}
 
-  public void rate(String title, int rating) {
-    ArrayList<LibraryNode> books = findByTitle(title);
-    if (books.size() >= 1) {
-      LibraryNode node = books.get(0);
-      node.setRating(rating);
-    }
-  }
-  
-  /**
-   * @pre
-   * option = [0 .. 3]
-   */
-  public ArrayList<LibraryNode> getBooks(int option) {
-    ArrayList<LibraryNode> sorted;
-    if (option == 0) {
-      sorted = (ArrayList<LibraryNode>)library.clone();
-      Collections.sort(sorted, new LibraryNode.compareByTitle());
-    } else if (option == 1) {
-      sorted = (ArrayList<LibraryNode>)library.clone();
-      Collections.sort(sorted, new LibraryNode.compareByAuthor());
-    } else if (option == 2) {
-      sorted = getReadBooks();
-      Collections.sort(sorted, new LibraryNode.compareByTitle());
-    } else {
-      sorted = getUnreadBooks();
-      Collections.sort(sorted, new LibraryNode.compareByTitle());
-    }
-    return sorted;
-  }
+	public void rate(String title, int rating) {
+		ArrayList<LibraryNode> books = findByTitle(title);
+		if (books.size() >= 1) {
+			LibraryNode node = books.get(0);
+			node.setRating(rating);
+		}
+	}
 
-  private ArrayList<LibraryNode> getReadBooks() {
-    ArrayList<LibraryNode> found = new ArrayList<>();
-    for (LibraryNode node : library) {
-      if (node.getIsRead()) {
-        found.add(node);
-      }
-    }
-    return found;
-  }
+	/**
+	 * @pre option = [0 .. 3]
+	 */
+	public ArrayList<LibraryNode> getBooks(int option) {
+		ArrayList<LibraryNode> sorted;
+		if (option == 0) {
+			sorted = (ArrayList<LibraryNode>) library.clone();
+			Collections.sort(sorted, new LibraryNode.compareByTitle());
+		} else if (option == 1) {
+			sorted = (ArrayList<LibraryNode>) library.clone();
+			Collections.sort(sorted, new LibraryNode.compareByAuthor());
+		} else if (option == 2) {
+			sorted = getReadBooks();
+			Collections.sort(sorted, new LibraryNode.compareByTitle());
+		} else {
+			sorted = getUnreadBooks();
+			Collections.sort(sorted, new LibraryNode.compareByTitle());
+		}
+		return sorted;
+	}
 
-  private ArrayList<LibraryNode> getUnreadBooks() {
-    ArrayList<LibraryNode> found = new ArrayList<>();
-    for (LibraryNode node : library) {
-      if (!node.getIsRead()) {
-        found.add(node);
-      }
-    }
-    return found;
-  }
+	private ArrayList<LibraryNode> getReadBooks() {
+		ArrayList<LibraryNode> found = new ArrayList<>();
+		for (LibraryNode node : library) {
+			if (node.getIsRead()) {
+				found.add(node);
+			}
+		}
+		return found;
+	}
 
-  public LibraryNode suggestRead() {
-    Random rand = new Random();
-    ArrayList<LibraryNode> unreadBooks = getUnreadBooks();
-    LibraryNode selected = unreadBooks.get(Math.abs(rand.nextInt()) % unreadBooks.size());
-    return selected;
-  }
-  
-  /**
-   * @param name of the file containing the books to be added
-   * creates and adds LibraryNodes from the input file to the library
-   */
-  public void addBooks(String fileName) {
-	  Scanner s = new Scanner(fileName);
-	  ArrayList<String> titles = new ArrayList<String>();
-	  ArrayList<String> authors = new ArrayList<String>();
-	  
-	  while (s.hasNext()) {
-		  String currLine = s.next();
-		  titles.add(currLine.substring(0, currLine.indexOf(';')));
-		  authors.add(currLine.substring(currLine.indexOf(';')+1));
-	  }
-	  
-	  for (int i = 0; i < titles.size(); i++) 
-		  this.addNode(titles.get(i), authors.get(i));
-	  
-	  s.close();
-  }
+	private ArrayList<LibraryNode> getUnreadBooks() {
+		ArrayList<LibraryNode> found = new ArrayList<>();
+		for (LibraryNode node : library) {
+			if (!node.getIsRead()) {
+				found.add(node);
+			}
+		}
+		return found;
+	}
+
+	public LibraryNode suggestRead() {
+		Random rand = new Random();
+		ArrayList<LibraryNode> unreadBooks = getUnreadBooks();
+		LibraryNode selected = unreadBooks.get(Math.abs(rand.nextInt()) % unreadBooks.size());
+		return selected;
+	}
+
+	/**
+	 * @param name of the file containing the books to be added creates and adds
+	 *             LibraryNodes from the input file to the library
+	 */
+	public void addBooks(String fileName) {
+		Scanner s = new Scanner(fileName);
+		ArrayList<String> titles = new ArrayList<String>();
+		ArrayList<String> authors = new ArrayList<String>();
+
+		while (s.hasNext()) {
+			String currLine = s.next();
+			titles.add(currLine.substring(0, currLine.indexOf(';')));
+			authors.add(currLine.substring(currLine.indexOf(';') + 1));
+		}
+
+		for (int i = 0; i < titles.size(); i++)
+			this.addNode(titles.get(i), authors.get(i));
+
+		s.close();
+	}
 }
