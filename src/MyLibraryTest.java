f50825396ea66e5be7095f8c9ef53ffe4aaf211e
Carson <49758625+CarsonHeyman@users.noreply.github.com>
2024-11-15 20:44:16 -0700

    Finished the tests for the Model and Controller.

--- src/MyLibraryTest.java
+++ src/MyLibraryTest.java
@@ -1,6 +1,15 @@
+/**
+ * @author Logan Anderson (username: logananderson), Carson Heyman (username: cheyman)
+ * 
+ * Runs various tests for the entire MyLibrary project (besides UIs)
+ */
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 import org.junit.Test;
 
@@ -68,15 +77,131 @@ public class MyLibraryTest {
 		l1.setRating(5);
 		assertEquals("birds, by cats, Read, Rating: 5/5", l1.toString());
 	}
-	/**
-	 * MyLibraryModel.java Tests
-	 */
 	
+	/**
+	 * MyLibraryConroller.java and MyLibraryModel.java Tests
+	 */
 	MyLibraryModel m = new MyLibraryModel();
+	MyLibraryController c = new MyLibraryController(m);
 	
+	@Test
+	public void testSearchOpt1() throws FileNotFoundException {
+		c.addBooks("books.txt");
+		ArrayList<LibraryNode> search = c.search(1, "the genius of birds");
+		assertEquals(search.get(0).toString(), "the genius of birds, by jennifer ackerman, Unread, Rating: unrated");
+	}
 	
+	@Test
+	public void testSearchOpt2() {
+		c.addBook("the genius of birds", "jennifer ackerman");
+		ArrayList<LibraryNode> search = c.search(2, "jennifer ackerman");
+		assertEquals(search.get(0).toString(), "the genius of birds, by jennifer ackerman, Unread, Rating: unrated");
+	}
 	
-	/**
-	 * MyLibraryConroller.java Tests
-	 */
+	@Test
+	public void testSearchOpt3() {
+		c.addBook("the genius of birds", "jennifer ackerman");
+		c.rate("the genius of birds", 4);
+		ArrayList<LibraryNode> search = c.search(3, "4");
+		assertEquals(search.get(0).toString(), "the genius of birds, by jennifer ackerman, Unread, Rating: 4/5");
+	}
+	
+	@Test
+	public void testSetToRead() {
+		c.addBook("test book", "test author");
+		c.setToRead("test book");
+		ArrayList<LibraryNode> search = c.search(1, "test book");
+		assertEquals(search.get(0).toString(), "test book, by test author, Read, Rating: unrated");
+	}
+	
+	@Test
+	public void testGetBooksOpt1() {
+		c.addBook("a book", "a test author");
+		c.addBook("b book", "b test author");
+		c.addBook("c book", "c test author");
+		c.addBook("d book", "a test author");
+		
+		c.setToRead("b book");
+		c.setToRead("d book");
+		
+		ArrayList<LibraryNode> books = c.getBooks(1);
+		
+		assertEquals(books.get(0).toString(), "a book, by a test author, Unread, Rating: unrated");
+		assertEquals(books.get(1).toString(), "b book, by b test author, Read, Rating: unrated");
+		assertEquals(books.get(2).toString(), "c book, by c test author, Unread, Rating: unrated");
+		assertEquals(books.get(3).toString(), "d book, by a test author, Read, Rating: unrated");
+	}
+	
+	@Test
+	public void testGetBooksOpt2() {
+		c.addBook("a book", "a test author");
+		c.addBook("b book", "b test author");
+		c.addBook("c book", "c test author");
+		c.addBook("d book", "a test author");
+		
+		c.setToRead("b book");
+		c.setToRead("d book");
+		
+		ArrayList<LibraryNode> books = c.getBooks(2);
+		
+		assertEquals(books.get(0).toString(), "a book, by a test author, Unread, Rating: unrated");
+		assertEquals(books.get(1).toString(), "d book, by a test author, Read, Rating: unrated");
+		assertEquals(books.get(2).toString(), "b book, by b test author, Read, Rating: unrated");
+		assertEquals(books.get(3).toString(), "c book, by c test author, Unread, Rating: unrated");
+	}
+	
+	@Test
+	public void testGetBooksOpt3() {
+		c.addBook("a book", "a test author");
+		c.addBook("b book", "b test author");
+		c.addBook("c book", "c test author");
+		c.addBook("d book", "a test author");
+		
+		c.setToRead("b book");
+		c.setToRead("d book");
+		
+		ArrayList<LibraryNode> books = c.getBooks(3);
+		
+		assertEquals(books.get(0).toString(), "b book, by b test author, Read, Rating: unrated");
+		assertEquals(books.get(1).toString(), "d book, by a test author, Read, Rating: unrated");
+	}
+	
+	@Test
+	public void testGetBooksOpt4() {
+		c.addBook("a book", "a test author");
+		c.addBook("b book", "b test author");
+		c.addBook("c book", "c test author");
+		c.addBook("d book", "a test author");
+		
+		c.setToRead("b book");
+		c.setToRead("d book");
+		
+		ArrayList<LibraryNode> books = c.getBooks(4);
+		
+		assertEquals(books.get(0).toString(), "a book, by a test author, Unread, Rating: unrated");
+		assertEquals(books.get(1).toString(), "c book, by c test author, Unread, Rating: unrated");
+	}
+	
+	@Test
+	public void testSuggestRead() {
+		c.addBook("b book", "b test author");
+		c.addBook("c book", "c test author");
+		c.addBook("d book", "a test author");
+		
+		c.setToRead("b book");
+		c.setToRead("d book");
+		
+		LibraryNode suggestion;
+		
+		// There's a lot of the same assertion just because this includes randomness
+		suggestion = c.suggestRead();
+		assertEquals(suggestion.toString(), "c book, by c test author, Unread, Rating: unrated");
+		suggestion = c.suggestRead();
+		assertEquals(suggestion.toString(), "c book, by c test author, Unread, Rating: unrated");
+		suggestion = c.suggestRead();
+		assertEquals(suggestion.toString(), "c book, by c test author, Unread, Rating: unrated");
+		suggestion = c.suggestRead();
+		assertEquals(suggestion.toString(), "c book, by c test author, Unread, Rating: unrated");
+	}
+	
 }
